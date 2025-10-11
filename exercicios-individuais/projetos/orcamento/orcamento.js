578315d43cafbddf1282f48e104c6261f2f4be62
jesset27 <jessewillian0@gmail.com>
2025-10-11 09:22:44 -0300

    Atividade Orçamento

--- exercicios-individuais/projetos/orcamento/orcamento.js
+++ exercicios-individuais/projetos/orcamento/orcamento.js
@@ -5,21 +5,23 @@ const PRECO_DESIGN_ADICIONAL = 1000;
 const inputPaginas = document.querySelector("#qtd-paginas");
 const inputDesconto = document.querySelector("#desconto");
 const checkboxDesign = document.querySelector("#inclui-design");
-const inputPrazo = document.querySelector("#prazo-entrega"); 
+const inputPrazo = document.querySelector("#prazo-entrega");
 const resumoSubtotal = document.querySelector("#resumo-subtotal");
-const resumoAdicional = document.querySelector("#resumo-adicional"); 
-const resumoUrgencia = document.querySelector("#resumo-urgencia"); 
+const resumoAdicional = document.querySelector("#resumo-adicional");
+const resumoUrgencia = document.querySelector("#resumo-urgencia");
 const resumoDesconto = document.querySelector("#resumo-desconto");
-const resumoTotal = document.querySelector("#resumo-total"); 
+const resumoTotal = document.querySelector("#resumo-total");
+const inputMensalidade = document.querySelector("#mensalidade");
+const resumoMensalidade = document.querySelector("#resumo-mensalidade");
 
 const calcularSubtotal = (quantidade) => quantidade * PRECO_POR_PAGINA;
 const calcularValorDesconto = (valor, porcentagem) => valor * (porcentagem / 100);
-function calcularTaxaDeUrgencia(valor, prazo){
+function calcularTaxaDeUrgencia(valor, prazo) {
     if (prazo < 5) {
-       return valor * 0.1
+        return valor * 0.1
     } else if (prazo >= 5 && prazo < 15) {
         return valor * 0.05;
-    }else{
+    } else {
         return 0;
     }
 }
@@ -31,4 +33,26 @@ function atualizarOrcamento() {
     const designIncluido = checkboxDesign.checked;
     const subtotal = calcularSubtotal(qtdPaginas);
     const adicionalDesign = designIncluido ? PRECO_DESIGN_ADICIONAL : 0;
+    const mensalidade = Number(inputMensalidade.value);
+
+
+    const taxaUrgencia = calcularTaxaDeUrgencia(subtotal + adicionalDesign, prazo);
+    const valorDesconto = calcularValorDesconto((subtotal + adicionalDesign + taxaUrgencia), porcentagemDesconto);
+    const total = (subtotal + adicionalDesign + taxaUrgencia) - valorDesconto;
+    resumoSubtotal.textContent = formatarValor(subtotal);
+    resumoAdicional.textContent = formatarValor(adicionalDesign);
+    resumoUrgencia.textContent = formatarValor(taxaUrgencia);
+    resumoTotal.textContent = formatarValor(total);
+    resumoMensalidade.textContent = formatarValor(mensalidade);
 }
+const todosInputs = [inputPaginas, inputPrazo, inputDesconto, checkboxDesign, inputMensalidade];
+const formatarValor = valor => valor.toLocaleString("pt-BR",
+    {
+        style: "currency",
+        currency: "BRL"
+    }
+);
+todosInputs.forEach(input => {
+    input.addEventListener('input', atualizarOrcamento);
+});
+document.addEventListener("DOMContentLoaded", atualizarOrcamento);
